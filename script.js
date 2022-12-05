714155b93cace773a0ad5a0459dbd5da65f97b39
Airat <mandarov@mail.ru>
2022-12-05 14:52:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+//  alert("Hello");
+// const result = confirm("Are you here?");
+// const answer = prompt("Вам есть 18 ?", "");
+// console.log(typeof answer);
+let incr = 10,
+  decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
